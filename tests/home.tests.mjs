b2a120ef5b5cf94d5208bd36f6d1f2a8735f3a18
Lachezar Todorov <121232198+lachezartdrv@users.noreply.github.com>
2025-08-04 14:17:27 +0300

    Update home.tests.mjs

--- tests/home.tests.mjs
+++ tests/home.tests.mjs
@@ -3,13 +3,13 @@ import fetch from 'node-fetch';
 
 suite('Home page', function() {
   test('Page title', async function() {
-    let res = await fetch("http://localhost:8888/");
+    let res = await fetch("http://localhost:8080/");
     let body = await res.text();
     assert.ok(body.includes("<h1>Students Registry</h1>"));
   });
   
   test('Students count', async function() {
-    let res = await fetch("http://localhost:8888/");
+    let res = await fetch("http://localhost:8080/");
     let body = await res.text();
     assert.ok(body.includes("Registered students: <b>2</b>"));
   });
