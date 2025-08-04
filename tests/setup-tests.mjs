3907da358fbb8cd5f97ec0a16685140770b8ac7f
Lachezar Todorov <lachezar.todorov15@gmail.com>
2025-08-04 13:21:09 +0300

    Initial commit

---
+++ tests/setup-tests.mjs
@@ -0,0 +1,29 @@
+let server;
+
+setup(async function () {
+  const students = [
+    { name: "Steve", email: "steve@gmail.com" },
+    { name: "Tina", email: "tina@yahoo.com" }
+  ];
+
+  const expressModule = await import('express');
+  const httpModule = await import('http');
+  const bodyParserModule = await import('body-parser');
+  const studentsController = await import('../controllers/students-controller.js');
+
+  const express = expressModule.default;
+  const app = express();
+
+  app.set('view engine', 'pug');
+  app.use(bodyParserModule.default.urlencoded({ extended: true }));
+
+  studentsController.setup(app, students);
+  server = httpModule.createServer(app);
+  server.listen(8888);
+});
+
+teardown(async function () {
+  if (server && server.close) {
+    await server.close();
+  }
+});
